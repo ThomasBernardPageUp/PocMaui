f25aea0eaf0c546079463a6bec9a1b7c5425444e
Thomas BERNARD <tbernard@pageup.fr>
2022-04-04 10:08:02 +0200

    First page

---
+++ PocMaui/PocMaui/ViewModels/ColorPickerViewModel.cs
@@ -0,0 +1,59 @@
+﻿using PocMaui.ViewModels.Base;
+
+namespace PocMaui.ViewModels
+{
+    public class ColorPickerViewModel : BaseViewModel
+    {
+
+        #region CTOR
+        public ColorPickerViewModel()
+        {
+        }
+        #endregion
+
+        #region Props
+
+        public string HexaColorString => $"{FrameColor.ToHex()}";
+
+        private int _redSliderValue;
+
+        public int RedSliderValue
+        {
+            get => _redSliderValue;
+            set 
+            { 
+                _redSliderValue = value;
+                NotifyPropertyChanged(nameof(FrameColor));
+                NotifyPropertyChanged(nameof(HexaColorString));
+            }
+        }
+
+        private int _greenSliderValue;
+        public int GreenSliderValue
+        {
+            get => _greenSliderValue;
+            set
+            {
+                _greenSliderValue = value;
+                NotifyPropertyChanged(nameof(FrameColor));
+                NotifyPropertyChanged(nameof(HexaColorString));
+            }
+        }
+
+        private int _blueSliderValue;
+        public int BlueSliderValue
+        {
+            get => _blueSliderValue;
+            set
+            {
+                _blueSliderValue = value;
+                NotifyPropertyChanged(nameof(HexaColorString));
+                NotifyPropertyChanged(nameof(FrameColor));
+            }
+        }
+
+        public Color FrameColor  => Color.FromRgb(RedSliderValue, GreenSliderValue, BlueSliderValue);  
+        #endregion
+
+    }
+}
--- PocMaui/PocMaui/Views/ColorPickerPage.xaml.cs
+++ PocMaui/PocMaui/Views/ColorPickerPage.xaml.cs
@@ -1,9 +1,14 @@
+using PocMaui.ViewModels;
+
 namespace PocMaui;
 
 public partial class ColorPickerPage : ContentPage
 {
 	public ColorPickerPage()
 	{
+		// Use ColorPickerViewModel to bind
+		this.BindingContext = new ColorPickerViewModel();
+
 		InitializeComponent();
 	}
 }
